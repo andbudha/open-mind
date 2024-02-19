88e2284a02907816078052470b9f824a14076ef5
andbudha <andreibartov@gmail.com>
2024-02-19 15:20:44 +0100

    Header comp created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Header } from './components/Header/Header';
 const styles = {
   main: `flex justify-center h-dvh`,
   container: `center container xl bg-[#f0f9ff] h-full`,
@@ -6,7 +7,9 @@ const styles = {
 function App() {
   return (
     <div className={styles.main}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <Header />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/logo/logo.png';
+const styles = {
+  container: `center container xl bg-[#211951] h-28 flex justify-between items-center`,
+  logo: `pl-6`,
+  btn_box: `pr-6 h-12 flex items-center`,
+  btn: `bg-[#FF004D] py-2 px-4 rounded `,
+};
+export const Header = () => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <img className={styles.logo} src={logo} alt="logo image" />
+      </div>
+      <div className={styles.btn_box}>
+        <button className={styles.btn}>Add Blog</button>
+      </div>
+    </div>
+  );
+};
