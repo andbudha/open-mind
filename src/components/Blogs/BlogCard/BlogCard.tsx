868f675e69701e48d763e137732c9b7236086407
andbudha <andreibartov@gmail.com>
2024-02-20 13:27:04 +0100

    BlogCard comp updated styles added

--- src/components/Blogs/BlogCard/BlogCard.tsx
+++ src/components/Blogs/BlogCard/BlogCard.tsx
@@ -1,35 +1,51 @@
+import { LuThumbsUp, LuThumbsDown } from 'react-icons/lu';
+
 const styles = {
-  blog_card: `w-80 h-96 bg-[#fff] border-zinc-600 shadow-md shadow-zinc-600 rounded mt-2`,
-  img_box: `w-80 h-40 bg-[#fff7ed] rounded`,
+  blog_card: `w-80 h-96 bg-[#fff] border-zinc-600 shadow-sm shadow-zinc-600 rounded-md my-6`,
+  img_box: `w-80 h-40 bg-[#fff7ed]`,
+  img: `w-full h-40 rounded-t-md`,
   info_box: `p-4 tracking-wide`,
-  title_box: `text-xl font-black`,
+  title_box: `text-lg font-medium text-slate-500 mb-2`,
   content_box: `line-clamp-3 text-left tracking-normal my-3 text-slate-500 text-sm leading-6`,
   author_box: `italic text-sm text-slate-500`,
-  footer: `w-full h-10 bg-[#fff7ed] flex items-center justify-between`,
-  thumb_box: ``,
+  footer: `w-full h-12  flex items-center justify-between`,
+  thumb_box: `flex justify-around items-center w-32`,
+  thumb_icon_box: `flex justify-around items-center w-12 h-8`,
+  thumb_icon: `cursor-pointer text-[#FC6736] transition ease-in-out hover:-translate-y-1 hover:scale-110 hover: duration-300`,
+  likes: `text-sm text-slate-500`,
   btn_box: ` tracking-normal text-sm `,
-  btn: `h-8 w-[100px] bg-[#FF004D] rounded flex justify-center items-center cursor-pointer`,
+  btn: `h-7 w-20 border border-orange-400 rounded text-[#FC6736] flex justify-center px-3 items-center cursor-pointer transition ease-in-out hover:-translate hover:scale-105 hover: duration-300`,
 };
 type BlogCard = {
   id: number;
   title: string;
   content: string;
   author: string;
+  image: string;
 };
-export const BlogCard = ({ id, title, content, author }: BlogCard) => {
+export const BlogCard = ({ id, title, content, author, image }: BlogCard) => {
   return (
     <div className={styles.blog_card}>
       <div className={styles.img_box}>
-        <img src="" alt="blog image" />
+        <img className={styles.img} src={image} alt="blog image" />
       </div>
       <div className={styles.info_box}>
         <div className={styles.title_box}>{title}</div>
         <div className={styles.author_box}>by: {author}</div>
         <div className={styles.content_box}>{content}...</div>
         <div className={styles.footer}>
-          <div className={styles.thumb_box}></div>
+          <div className={styles.thumb_box}>
+            <div className={styles.thumb_icon_box}>
+              <LuThumbsUp className={styles.thumb_icon} />
+              <div className={styles.likes}>{190}</div>
+            </div>
+            <div className={styles.thumb_icon_box}>
+              <LuThumbsDown className={styles.thumb_icon} />
+              <div className={styles.likes}>{120}</div>
+            </div>
+          </div>
           <div className={styles.btn_box}>
-            <div className={styles.btn}>Read Moore</div>
+            <div className={styles.btn}>Read</div>
           </div>
         </div>
       </div>
