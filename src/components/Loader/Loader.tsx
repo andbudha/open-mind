ed877d145c797e422157ffa41c9f4657f53e8c1a
andbudha <andreibartov@gmail.com>
2024-02-22 21:25:43 +0100

    Loader comp added&styled

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import styles from './Loader.module.css';
+
+const loader = {
+  main: `absolute h-full w-full bg-transparent flex justify-center items-center opacity-100 `,
+};
+export const Loader = () => {
+  return (
+    <div className={loader.main}>
+      <span className={styles.loader}></span>
+    </div>
+  );
+};
