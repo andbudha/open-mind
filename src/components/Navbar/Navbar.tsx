959b5c8db126088af834bccc9f8bee30385c66f2
andbudha <andreibartov@gmail.com>
2024-02-28 11:47:11 +0100

    Navbar active buttons restyled

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,12 +5,11 @@ import { NavLink } from 'react-router-dom';
 
 const navbar = {
   main: `h-24 w-full bg-[#f3f4f6] flex justify-between items-center`,
-  logo_box: `h-20 w-48 flex justify-start items-center text-3xl italic tracking-wider text-[#FC6736] `,
+  logo_box: `h-20 w-48 flex justify-start items-center text-3xl italic tracking-wider text-[#FBA834] `,
   logo: `pl-10`,
-  btn_box: `pr-10 h-20 w-[500px] flex items-center justify-end w-full italic tracking-wider text-[#FC6736] `,
-  btn: `py-1 px-4 ml-3 flex justify-end items-center border border-orange-300 rounded-full cursor-pointer  w-34 transition ease-in-out hover:-translate-y hover:scale-110 hover: duration-300`,
+  btn_box: `pr-10 h-20 w-[500px] flex items-center justify-end w-full italic tracking-wider text-[#FBA834] `,
+  btn: `py-1 px-4 ml-3 flex justify-end items-center border border-[#FBA834] rounded-full cursor-pointer  w-34 transition ease-in-out hover:-translate-y hover:scale-110 hover:duration-300 hover:bg-[#FBA834] hover:text-[#fff]`,
   icon: `ml-2 h-5 w-5`,
-  active: ` text-purple-100 visited:text-purple-600`,
 };
 export const Navbar = () => {
   return (
@@ -21,14 +20,35 @@ export const Navbar = () => {
         </NavLink>
       </div>
       <div className={navbar.btn_box}>
-        <NavLink to="/" className={navbar.btn}>
+        <NavLink
+          to="/"
+          className={`${navbar.btn}`}
+          style={({ isActive }) => ({
+            background: isActive ? '#FBA834' : '',
+            color: isActive ? '#fff' : '',
+          })}
+        >
           <div>home</div> <AiOutlineHome className={navbar.icon} />
         </NavLink>
-        <NavLink to="blogform" className={navbar.btn}>
+        <NavLink
+          to="blogform"
+          className={`${navbar.btn}`}
+          style={({ isActive }) => ({
+            background: isActive ? '#FBA834' : '',
+            color: isActive ? '#fff' : '',
+          })}
+        >
           <div>add blog </div>
           <LuPenSquare className={navbar.icon} />
         </NavLink>
-        <NavLink to="login" className={navbar.btn}>
+        <NavLink
+          to="login"
+          className={navbar.btn}
+          style={({ isActive }) => ({
+            background: isActive ? '#FBA834' : '',
+            color: isActive ? '#fff' : '',
+          })}
+        >
           <div>log in</div>
           <AiOutlineLogin className={navbar.icon} />
         </NavLink>
